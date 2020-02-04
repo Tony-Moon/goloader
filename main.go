4a6da5e26e7696062b15ac5004bf17981f6acd20
Tony-Moon <anthonyowenmoon@gmail.com>
2020-02-04 12:48:11 -0500

    can donwload one specific file

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+var fileURL string
 
 func main() {
-	fmt.Println("Hello goloader")
+	fileURL = "https://golangcode.com/images/avatar.jpg"
+	if err := DownloadFile("avatar.jpg", fileURL); err != nil {
+		panic(err)
+	}
+}
+
+// DownloadFile will download a url to a locoal file.
+func DownloadFile(filepath string, url string) error {
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create a file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write to the file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
